767ab840ecb9cbd4d33031be042c0ba5e41d82dd
gitjanco <jam6nnu@virginia.edu>
2023-11-28 04:04:04 -0500

    updated index, styles, and connect4js to add win and game over logic

--- node/connect4.js
+++ node/connect4.js
@@ -1,123 +1,142 @@
-const http = require('http')
-const url = require('url')
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 const server = http.createServer();
 
 server.on('request', (request, response) => {
+    const { method, url: requestUrl } = request;
+    console.log("Logging " + method + " url: " + requestUrl);
+    
     response.writeHead(200, { 'Content-Type': 'application/json' });
-    let q = url.parse(request.url, true);
+    let q = url.parse(requestUrl, true);
     response.write(handleUserAction(q.pathname, q.query));
     response.end();
 });
-
-server.on('request', (request, response) => {
-    const { method, url } = request;
-    console.log(" logging " + method + " url: " + url);
-})
 
 server.on('error', error => console.error(error.stack));
 
 server.listen(3000, "127.0.0.1", () => console.log('Server running at http://127.0.0.1:3000/ - to stop server press Ctrl+C'));
 
+let gameboard = JSON.parse(fs.readFileSync('gameboard.json', 'utf-8'));
 let turn;
 let totalTurns;
-let col; 
 let isActive = true;
-let winner = false;
+let winner = null;
 
 function handleUserAction(urlString, queryString) {
     let clientUrl = urlString.split("/");
-    // What does my url look like? http://localhost:8080/api/gameboard
     if (clientUrl[1] == "startgame") {
         turn = 1;
         totalTurns = 0;
-        return JSON.stringify({ "turn": turn, "totalTurns": totalTurns});
+        isActive = true;
+        winner = null;
+        // Initialize gameboard
+        gameboard.board = gameboard.board.map(() => Array(gameboard.columns).fill(""));
+        return JSON.stringify({ "turn": turn, "totalTurns": totalTurns, "board": gameboard.board });
     }
     else if (clientUrl[1] == "gameboard") {
-        return "gameboard";
+        return JSON.stringify(gameboard.board);
     }
     else if (clientUrl[1] == "state") {
-        return JSON.stringify({ "turn": turn, "active": isActive, "totalTurns": winner});
-        // return {
-        //     "turn": turn,
-        //     "active": isActive,
-        //     "winner": winner
-        // }
+        return JSON.stringify({ "turn": turn, "active": isActive, "winner": winner });
     } 
-    else if (clientUrl[1] == "droptoken") {
-        console.log(queryString);
-        col = queryString.column;
-        totalTurns += 1;
-        turn = evenOrOdd(totalTurns);
-        
-
-        return JSON.stringify({"message": "Player " + turn + " played in column " + col + ".", "turn": turn, "totalTurns": totalTurns});
+    else if (clientUrl[1] == "droptoken" && isActive) {
+        let col = parseInt(queryString.column);
+        if (!isNaN(col) && col >= 0 && col < gameboard.columns) {
+            totalTurns += 1;
+            turn = evenOrOdd(totalTurns);
+            dropToken(col, turn);
+            winner = checkWin(gameboard.board);
+            if (winner) {
+                isActive = false;
+                return JSON.stringify({ "message": `Player ${winner} wins!`, "turn": turn, "totalTurns": totalTurns, "winner": winner });
+            } else if (isBoardFull(gameboard.board)) {
+                isActive = false;
+                return JSON.stringify({ "message": "Draw!", "turn": turn, "totalTurns": totalTurns, "winner": winner });
+            } else {
+                return JSON.stringify({ "message": `Player ${turn} played in column ${col}.`, "turn": turn, "totalTurns": totalTurns });
+            }
+        } else {
+            return JSON.stringify({ "message": "Invalid column." });
+        }
     }
     else {
-        return "Invalid URL";
+        return JSON.stringify({ "message": "Invalid URL" });
     }
 }
 
 function evenOrOdd(num) {
-    if (num % 2 == 0) {
-        return 2;
-    }
-    else {
-        return 1;
+    return num % 2 === 0 ? 2 : 1;
+}
+
+function dropToken(col, playerToken) {
+    for (let row = gameboard.rows - 1; row >= 0; row--) {
+        if (!gameboard.board[row][col]) {
+            gameboard.board[row][col] = playerToken;
+            break;
+        }
     }
 }
 
-// 1. Let player make move
-// 2. Check if move is valid
-// 3. Check if move is winning move
-// 4. Check if board is full
-// 5. If not full, switch player
-// 6. If full, game is over
-// 7. If not full, go to step 1
+function isBoardFull(board) {
+    return board.every(row => row.every(cell => cell));
+}
 
-// initialize game variables
-// current_player = Player1
+function checkHorizontal(board) {
+  // Check each row
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[0].length - 3; c++) {
+      if (board[r][c] && board[r][c] === board[r][c+1] && board[r][c] === board[r][c+2] && board[r][c] === board[r][c+3]) {
+        return board[r][c]; // return the token of the player that won
+      }
+    }
+  }
+  return 0; // no winner found
+}
 
-// while true:
-    // Step 1: Let player make a move
-//     player_move = get_player_move(current_player)
+function checkVertical(board) {
+  // Check each column
+  for (let c = 0; c < board[0].length; c++) {
+    for (let r = 0; r < board.length - 3; r++) {
+      if (board[r][c] && board[r][c] === board[r+1][c] && board[r][c] === board[r+2][c] && board[r][c] === board[r+3][c]) {
+        return board[r][c]; // return the token of the player that won
+      }
+    }
+  }
+  return 0; // no winner found
+}
 
-     // Step 2: Check if the move is valid
-//     if is_valid_move(player_move):
-         // Step 3: Check if the move is a winning move
-//         if is_winning_move(player_move):
-//             display_winner(current_player)
-//             break
-//         else:
-             // Step 4: Check if the board is full
-//             if is_board_full():
-//                 display_draw()
-//                 break
-//             else:
-//                 // Step 5: If not full, switch player
-//                 switch_player()
+function checkDiagonal(board) {
+  // Check downward diagonals
+  for (let r = 0; r < board.length - 3; r++) {
+    for (let c = 0; c < board[0].length - 3; c++) {
+      if (board[r][c] && board[r][c] === board[r+1][c+1] && board[r][c] === board[r+2][c+2] && board[r][c] === board[r+3][c+3]) {
+        return board[r][c]; // return the token of the player that won
+      }
+    }
+  }
 
-//     // Step 7: If not full, go to step 1
-//     else:
-//         display_invalid_move_message()
+  // Check upward diagonals
+  for (let r = 3; r < board.length; r++) {
+    for (let c = 0; c < board[0].length - 3; c++) {
+      if (board[r][c] && board[r][c] === board[r-1][c+1] && board[r][c] === board[r-2][c+2] && board[r][c] === board[r-3][c+3]) {
+        return board[r][c]; // return the token of the player that won
+      }
+    }
+  }
+
+  return 0; // no winner found
+}
+
+function checkWin(board) {
+    // Check for a win in all directions
+    return checkHorizontal(board) || checkVertical(board) || checkDiagonal(board);
+}
 
 
 
-/////////////////////////// Notes ///////////////////////////
-//if (clientUrl[1] == "gameboard") {
-    //     console.log("gameboard requested");
-    //     return "gameboard";
-    // } else if (clientUrl[1] == "move") {
-    //     console.log("move");
-    //     return "move";
-    // } else if (clientUrl[1] == "data") {
-    //     let stringData = "Jurassic 5";
-    //     let arrayData = ["Run DMC", "LL Cool J", "Big Daddy Kane"]
-    //     let dataContainer = {}
-    //     dataContainer['oneGroup'] = stringData;
-    //     dataContainer['twoGroup'] = arrayData;
-    //     return JSON.stringify(dataContainer);
-    // } else {
-    //     return "";
-    // }
-/////////////////////////////////////////////////////////////
+// Start the server
+server.listen(3000, () => {
+    console.log('Connect4 server is running on http://localhost:3000');
+});  
